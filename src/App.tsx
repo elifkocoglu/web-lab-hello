f87098c74940a4022b4178afeb277e4469a182de
Elif Koçoğlu <elif.kocoglu74@gmail.com>
2026-03-02 19:57:38 +0300

    feat: add student info to app

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ function App() {
     <div>
       <h1>Web Tasarımı ve Programlama</h1>
       <h2>LAB-1: Geliştirme Ortamı Kurulumu</h2>
+      <p>Ad Soyad: Elif Koçoğlu</p>
+<p>Öğrenci No: [230541015]</p>
     </div>
   )
 }
